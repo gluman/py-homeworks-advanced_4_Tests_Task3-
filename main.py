d62ecdb2318d76f1695e2a5a6a53547664541ba3
Glumov Andrey <gluman@yandex.ru>
2023-04-04 22:33:48 +0300

    04.04.2023 first commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
-import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 url = 'https://www.ya.ru'
 # 'C:\\Users\\selenium_pyton\'
 path = '/'
 driver = webdriver.Chrome(executable_path='')
 
-
-gfg
