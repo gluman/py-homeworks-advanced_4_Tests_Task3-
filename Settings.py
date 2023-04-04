e70a52b534c14e1578121eb8ee93f3acb1230b38
Glumov Andrey <gluman@yandex.ru>
2023-04-04 23:28:44 +0300

    04.04.2023 driver ready

---
+++ Settings.py
@@ -0,0 +1,2 @@
+chrome_bin_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+webdriver_location = 'C:\chromedriver\chromedriver.exe'
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 # from webdriver_manager.chrome import ChromeDriverManager
+from Settings import chrome_bin_location
+from Settings import webdriver_location
 
 url = 'https://www.ya.ru'
-# 'C:\\Users\\selenium_pyton\'
-path = '/'
-driver = webdriver.Chrome(executable_path='')
 
+
+options = Options()
+options.binary_location = chrome_bin_location
+driver = webdriver.Chrome(chrome_options=options, executable_path=webdriver_location)
+
+driver.get(url)
+print(driver.title)
+driver.close()
